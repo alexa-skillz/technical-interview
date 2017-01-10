da9f34ac6e52dfd4a7bc596f88ce5bfa312b9891
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-10 11:18:45 -0800

    outline alexa skill

---
+++ src/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const APP_ID = undefined;
+const AlexaSkill = require('./AlexaSkill.js');
+
+const TechQuestion = function() {
+  AlexaSkill.call(this, APP_ID);
+};
+
+TechQuestion.prototype = Object.create(AlexaSkill.prototype);
+TechQuestion.prototype.constructor = TechQuestion;
+
+TechQuestion.prototype.eventHandlers.onSessionStarted = function() {
+  // TODO: takes 2 parms of sessionStartedRequest and session
+};
+
+TechQuestion.prototype.eventHandlers.onLaunch = function() {
+  // TODO: takes 3 params of LaunchRequest, session, and response
+};
+
+TechQuestion.prototype.eventHandlers.onSessionStarted = function() {
+  // TODO: takes 2 parms of sessionEndedRequest and session
+};
+
+TechQuestion.prototype.intentHandlers = {
+  // TODO: add GetQuestionIntent
+  // TODO: add DontKnowIntent
+  // TODO: GiveHintIntent (STRETCH GOAL)
+  // TODO: ShowAnswerIntent (STRETCH GOAL)
+  // TODO: AMAZON.StartOverIntent
+  // TODO: AMAZON.RepeatIntent
+  // TODO: AMAZON.HelpIntent
+  // TODO: AMAZON.StopIntent
+  // TODO: AMAZON.CancelIntent
+};
+
+exports.handler = function(event, context) {
+  let question = new TechQuestion();
+  question.execute(event, context);
+};
